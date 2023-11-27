5c4a1ef3f151eb7ad2890fb354d6f7a6c280fc64
beurokrat <beurokrat@gmail.com>
2023-11-27 16:18:32 +0530

    netlify fix

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Container from './container';
 
 const Header = () => {
   return (
-    <header className='fixed shadow-md top-0 bg-white bg-opacity-50 z-30 backdrop-blur-lg backdrop-filter w-full'>
+    <header className='fixed shadow-md top-0 bg-grey-800 bg-opacity-70 z-30 backdrop-blur-lg backdrop-filter w-full'>
       <Container>
         <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-5 mt-5'>
           <Link href='/' className='hover:underline'>
